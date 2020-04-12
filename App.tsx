fc84f478fe8d153a80226d5100a68e489016bbc5
mohsinjk <mohsinjk@hotmail.com>
2020-04-12 20:16:25 +0200

    Add text input

--- App.tsx
+++ App.tsx
@@ -1,19 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//This is an example code to show Image Icon in TextInput// 
+import React, { Component } from 'react';
+//import react in our code.
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+import { StyleSheet, View, TextInput, Image } from 'react-native';
+//import all the components we are going to use. 
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.SectionStyle}>
+          <Image
+            source={{uri:'https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_username.png',}}
+            style={styles.ImageStyle}
+          />
+
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Enter Your Name Here"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+         <View style={styles.SectionStyle}>
+          <Image
+            source={{uri:'https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_phone.png',}}
+            style={styles.ImageStyle}
+          />
+
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Enter Your Mobile No Here"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+
+  SectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 10,
+   elevation: 5
+  },
+
+  ImageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+    alignItems: 'center',
   },
 });
